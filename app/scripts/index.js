93b837ed3932c14558cbc26d27a8af93005257f2
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-02-15 09:09:23 -0500

    fixed submit input

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -8,6 +8,12 @@ var pizzaTemplate = require('../templates/pizza.hbs');
 
 var todaysPizza = new model.PizzaCollection();
 
+
+var pizzaName,
+    pizzaIngr,
+    pizzaType,
+    pizzaNuts;
+
 console.log(todaysPizza);
 
 $(".form-group").html(formTemplate());
@@ -38,5 +44,15 @@ $('.button-class').on('click', function(event){
   $(".button-class").attr("disabled", true);
   todaysPizza.fetch();
 
+});
+
+$('#pizza-button').on('click', function(e) {
+    e.preventDefault();
+    pizzaName = $('#pizza-namer').val();
+    pizzaIngr = $('#pizza-ingr').val();
+    pizzaType = $('#pizza-type').val();
+    pizzaNuts = $('#pizza-nuts').val();
+
+   console.log(pizzaName, pizzaIngr, pizzaType, pizzaNuts);
 });
 //todaysLasagna.fetch();
